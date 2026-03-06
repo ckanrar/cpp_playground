ba267aee4298ae0d726e4c9d2b4efcf448dc3219
ckanrar <50054176+ckanrar@users.noreply.github.com>
2026-03-06 23:32:58 +0000

    CPP-2: Add private members to the header

--- include/rate_limiter.h
+++ include/rate_limiter.h
@@ -1,10 +1,15 @@
 #include <cstdint>
+#include <queue>
 
 namespace playground{
 class RateLimiter {
 public:
-  RateLimiter(std::int64_t timeInNanos, int numEntriesAllowedPerTimeSlice);
+  RateLimiter(std::int64_t timeSliceInNanos, int numEntriesAllowedPerTimeSlice);
   bool checkIfAllowed(std::int64_t currentTimestampNanos);
   void commit(std::int64_t currentTimestampNanos);
+private:
+  std::queue<std::int64_t> mCommittedTimestampNanos;
+  std::int64_t mTimeSliceInNanos;
+  int mNumEntriesAllowedPerTimeSlice;
 };
 }
